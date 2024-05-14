5d5e88032cab2080c486ffcf6e21e0efc7f52f5c
agigibairyn <ireneagigiba>
2024-05-14 15:13:31 +0200

    Create a phone number from a given array

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -19,6 +19,6 @@ function createPhoneNumber(numbers) {
     return  `(${firstThree}) ${secondThree}-${last}`
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+// console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
